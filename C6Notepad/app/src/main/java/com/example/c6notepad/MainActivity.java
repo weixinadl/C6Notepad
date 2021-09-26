296acd60444b790648d6c02a0ddd33534a89d453
xintaxs <36997074+xintaxs@users.noreply.github.com>
2021-09-26 17:32:09 +0800

    can i see it

--- C6Notepad/app/src/main/java/com/example/c6notepad/MainActivity.java
+++ C6Notepad/app/src/main/java/com/example/c6notepad/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.c6notepad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv;
+        Button btn;
     }
 }
